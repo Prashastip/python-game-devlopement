4666c8cf3e3753a1cb44f02d4e94e6783b34aabf
Prashastip <74597811+Prashastip@users.noreply.github.com>
2026-05-12 12:41:10 +0200

    Add files via upload

---
+++ pgzero and shapes/main.py
@@ -0,0 +1,27 @@
+import pgzrun
+import random
+
+WIDTH=300
+HEIGHT=300
+
+TITLE="Shapes"
+
+def draw():
+    r=255
+    g=56
+    b=random.randint(120,255)
+    width=WIDTH
+    height=HEIGHT-200
+    for i in range(20):
+        rect=Rect(0,0,width,height)
+        rect.center=WIDTH/2,HEIGHT/2
+        screen.draw.rect(rect,(r,g,b))
+        width-=10
+        height+=10
+        r-=10
+        g+=10
+
+def update():
+    pass
+
+pgzrun.go()
